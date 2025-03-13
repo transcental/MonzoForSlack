90ddd8d27e130d6d2f53e9f3373b9074abab0eab
Amber <amber@transcental.dev>
2025-03-13 10:41:37 +0000

    fix non existent merchant

--- abd/utils/starlette.py
+++ abd/utils/starlette.py
@@ -49,7 +49,7 @@ async def webhook(req: Request):
     match type:
         case "transaction.created":
             amount = data.get("amount")
-            merchant = data.get("merchant")
+            merchant = data.get("merchant", {})
             icon = merchant.get("logo")
             emoji = merchant.get("emoji")
             name = merchant.get("name")
